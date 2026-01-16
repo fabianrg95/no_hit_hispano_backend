760e556eb092719d9048caff0d90d96e09d98ca6
Fabian Mauricio Rodriguez Rodriguez <fabian.rg0801@gmail.com>
2026-01-16 11:34:21 -0500

    Se agrega el año al nombre de la hoja

--- src/sincronizacion_mensual.py
+++ src/sincronizacion_mensual.py
@@ -50,8 +50,8 @@ def obtener_informacion_excel(hoja=None) -> [PartidaExcel]:
 
         print(f"Obteniendo informacion de ({mes_actual}) y ({mes_anterior}) desde el excel.")
 
-        datos_actual = obtener_informacion_excel_por_hoja(mes_actual)
-        datos_anterior = obtener_informacion_excel_por_hoja(mes_anterior)
+        datos_actual = obtener_informacion_excel_por_hoja(mes_actual + " 2026")
+        datos_anterior = obtener_informacion_excel_por_hoja(mes_anterior + " 2026")
         datos_procesar = datos_anterior + datos_actual
     else:
         print(f"Obteniendo informacion de la hoja ({hoja}) desde el excel.")
